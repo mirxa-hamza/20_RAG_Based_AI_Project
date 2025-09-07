f54b29aa23b9bbc4c383b6757b4b5f258768b6ad
mirxa-hamza <mustafamirzahamza0@gmail.com>
2025-09-07 16:42:55 +0500

    Updated read_chunks.py

--- read_chunks.py
+++ read_chunks.py
@@ -2,6 +2,8 @@ import requests
 import os
 import json
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 def create_embedding(text_list):
     # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
@@ -36,3 +38,18 @@ for json_file in jsons:
 
 df = pd.DataFrame.from_records(my_dicts)
 print(df)
+
+incoming_query = input("Ask A Question :")
+question_embedding = create_embedding([incoming_query])[0]
+
+
+#Find cosine_similarities of question_embeddings with other embeddings
+# print(np.vstack(df['embedding'].values))
+# print(np.vstack(df['embedding'].shape))
+similarities = cosine_similarity(np.vstack(df['embedding']),[question_embedding]).flatten()
+print(similarities)
+top_results = 3
+max_indx = similarities.argsort()[::-1][0:top_results]
+print(max_indx)
+new_df = df.loc[max_indx]
+print(new_df[["title","number","text"]])
